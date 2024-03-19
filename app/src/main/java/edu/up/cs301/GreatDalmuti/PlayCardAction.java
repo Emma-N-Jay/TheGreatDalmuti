90a0bf72d770cf0f03970a599404e2af88abb9a6
DropT <best27@up.edu>
2024-03-19 16:26:24 -0700

    actions done please please please :)) ;3

--- app/src/main/java/edu/up/cs301/GreatDalmuti/PlayCardAction.java
+++ app/src/main/java/edu/up/cs301/GreatDalmuti/PlayCardAction.java
@@ -1,5 +1,7 @@
 package edu.up.cs301.GreatDalmuti;
 
+import java.util.ArrayList;
+
 import edu.up.cs301.GameFramework.actionMessage.GameAction;
 import edu.up.cs301.GameFramework.infoMessage.GameInfo;
 import edu.up.cs301.GameFramework.players.GamePlayer;
@@ -24,5 +26,22 @@ public class PlayCardAction extends GameAction {
         super(player);
         game.getTurn();
     }
+
+    public ArrayList<ArrayList<Integer>> play(int player, ArrayList<ArrayList<Integer>> decks, ArrayList<ArrayList<Integer>> selected){
+        for(int i = 0; i < decks.get(player).size(); i++){
+            if(selected.get(player).get(i) > 0){
+                decks.get(player).set(i, decks.get(player).get(i) - selected.get(player).get(i));
+            }
+        }
+        if(game.getTurn() == 4 ){
+            game.setTurn(1);
+        } else {
+            game.setTurn(game.getTurn() + 1);
+        }
+
+        return decks;
+    }
+
+
 
 }
