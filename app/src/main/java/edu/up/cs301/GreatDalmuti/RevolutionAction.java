33dce71f55f510dd207736a7c5a41904bd3e86c3
Emma-N-Jay <emma.nicole.jeppesen@gmail.com>
2024-03-19 16:51:31 -0700

    Revolu Fixed?

--- app/src/main/java/edu/up/cs301/GreatDalmuti/RevolutionAction.java
+++ app/src/main/java/edu/up/cs301/GreatDalmuti/RevolutionAction.java
@@ -27,17 +27,17 @@ public class RevolutionAction extends GameAction {
     //given that the player that has the jesters calls the revolution, carries out revolution
     public boolean revolution(int player, ArrayList<ArrayList<Integer>> cards){
         if(cards.get(player).get(13) == 2){
-            if(player == 3){
+            if(player == 2){
                 game.setExchangingTaxes(false);
-            } else if(player == 4){
+            } else if(player == 3){
 
                 //switches player 1 for 4 & 2 for 3
                 ArrayList<ArrayList<Integer>> newCards = null;
                     for(int j = 0; j < cards.get(1).size(); j++) {
-                        newCards.get(4).set(j, cards.get(1).get(j));
-                        newCards.get(3).set(j, cards.get(2).get(j));
-                        newCards.get(2).set(j, cards.get(3).get(j));
-                        newCards.get(1).set(j, cards.get(4).get(j));
+                        newCards.get(3).set(j, cards.get(0).get(j));
+                        newCards.get(2).set(j, cards.get(1).get(j));
+                        newCards.get(1).set(j, cards.get(2).get(j));
+                        newCards.get(0).set(j, cards.get(3).get(j));
                 }
             }
         }
