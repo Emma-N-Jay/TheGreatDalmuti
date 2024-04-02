6cd1566d618aa743bf28beae80dbca131ab470fb
alex <alex@burnsjohnston.com>
2024-04-02 14:46:24 -0700

    better names for smart and dumb ai

--- app/src/main/java/edu/up/cs301/GreatDalmuti/CDSmartAI.java
+++ app/src/main/java/edu/up/cs301/GreatDalmuti/CDSmartAI.java
@@ -14,6 +14,7 @@ import edu.up.cs301.GameFramework.infoMessage.GameInfo;
 import edu.up.cs301.GameFramework.players.GameComputerPlayer;
 import edu.up.cs301.GameFramework.utilities.Tickable;
 
+
 public class CDSmartAI extends GameComputerPlayer implements Tickable {
 
 	// INSTANCE VARIABLES **************************************************************************
