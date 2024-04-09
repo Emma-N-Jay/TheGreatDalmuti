df0c0a0ecd6d7aa07896dd6f1a751d786c041cef
Emma-N-Jay <emma.nicole.jeppesen@gmail.com>
2024-04-09 14:44:33 -0700

    Me trying and struggling to do surface view stuff

---
+++ app/src/main/java/edu/up/cs301/GreatDalmuti/surfaceDraw.java
@@ -0,0 +1,24 @@
+package edu.up.cs301.GreatDalmuti;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.view.SurfaceView;
+
+public class surfaceDraw extends SurfaceView {
+    public surfaceDraw(Context context) {
+        super(context);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas){
+
+        //Create a bitmap that contains image
+        Bitmap test = BitmapFactory.decodeResource(getResources(), R.drawable.abbess);
+
+        test = Bitmap.createScaledBitmap(test, 100, 100, false);
+
+        //canvas.drawBitmap(SurfaceView);
+    }
+}
