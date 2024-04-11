72129284fb40f4b5fda46151b1a422e14899083a
Emma-N-Jay <emma.nicole.jeppesen@gmail.com>
2024-04-11 11:08:57 -0700

    Test

--- app/src/main/java/edu/up/cs301/GreatDalmuti/surfaceDraw.java
+++ app/src/main/java/edu/up/cs301/GreatDalmuti/surfaceDraw.java
@@ -27,68 +27,88 @@ public class surfaceDraw extends SurfaceView {
     @Override
     protected void onDraw(Canvas canvas){
 
-        //set the purple color
-        purple.setColor(0xFF402264);
-        purple.setTextSize(50);
-        //purple.setStyle(Paint.Style.FILL);
-        this.setBackgroundColor(0Xffffffff);
 
 
-        //Create a bitmap that contains image
-        Bitmap back = BitmapFactory.decodeResource(getResources(), R.drawable.card_back);
-        Bitmap backleft = BitmapFactory.decodeResource(getResources(), R.drawable.card_leftside);
-        Bitmap backright = BitmapFactory.decodeResource(getResources(), R.drawable.card_rightside);
-        Bitmap dalmuti = BitmapFactory.decodeResource(getResources(), R.drawable.great_dalmuti);
+            //set the purple color
+            purple.setColor(0xFF402264);
+            purple.setTextSize(25);
+            this.setBackgroundColor(0Xffffffff);
+            purple.setStyle(Paint.Style.FILL);
 
 
-        //scale the bitmap
-        back = Bitmap.createScaledBitmap(back, 100, 150, false);
-        backleft = Bitmap.createScaledBitmap(backleft, 100, 150, false);
-        backright = Bitmap.createScaledBitmap(backright, 100, 150, false);
-        dalmuti = Bitmap.createScaledBitmap(dalmuti, 100, 100, false);
+            //Create a bitmap that contains image
+            //back of the cards
+            Bitmap back = BitmapFactory.decodeResource(getResources(), R.drawable.card_back);
+            Bitmap backleft = BitmapFactory.decodeResource(getResources(), R.drawable.card_leftside);
+            Bitmap backright = BitmapFactory.decodeResource(getResources(), R.drawable.card_rightside);
 
-        //draw the top hand
-        canvas.drawBitmap(back,450, 10, null);
-        canvas.drawBitmap(back,550, 10, null);
-        canvas.drawBitmap(back,650, 10, null);
-        canvas.drawBitmap(back,750, 10, null);
-        canvas.drawBitmap(back,850, 10, null);
-        canvas.drawBitmap(back,950, 10, null);
-        canvas.drawBitmap(back,1050, 10, null);
-        canvas.drawBitmap(back,1150, 10, null);
-        canvas.drawBitmap(back,1250, 10, null);
-        canvas.drawBitmap(back,1350, 10, null);
-
-        //draw the left players hand
-        canvas.drawBitmap(backleft,10, 100, null);
-        canvas.drawBitmap(backleft,10, 125, null);
-        canvas.drawBitmap(backleft,10, 150, null);
-        canvas.drawBitmap(backleft,10, 175, null);
-        canvas.drawBitmap(backleft,10, 200, null);
-        canvas.drawBitmap(backleft,10, 225, null);
-        canvas.drawBitmap(backleft,10, 250, null);
-        canvas.drawBitmap(backleft,10, 275, null);
-        canvas.drawBitmap(backleft,10, 300, null);
-        canvas.drawBitmap(backleft,10, 325, null);
+            //Actual cards
+            Bitmap dalmuti = BitmapFactory.decodeResource(getResources(), R.drawable.great_dalmuti);
+//            Bitmap dalmuti = BitmapFactory.decodeResource(getResources(), R.drawable.great_dalmuti);
+//            Bitmap dalmuti = BitmapFactory.decodeResource(getResources(), R.drawable.great_dalmuti);
+//            Bitmap dalmuti = BitmapFactory.decodeResource(getResources(), R.drawable.great_dalmuti);
+//            Bitmap dalmuti = BitmapFactory.decodeResource(getResources(), R.drawable.great_dalmuti);
+//            Bitmap dalmuti = BitmapFactory.decodeResource(getResources(), R.drawable.great_dalmuti);
+//            Bitmap dalmuti = BitmapFactory.decodeResource(getResources(), R.drawable.great_dalmuti);
 
 
-        //draw the right players hand
-        canvas.drawBitmap(backright,1785, 100, null);
-        canvas.drawBitmap(backright,1785, 125, null);
-        canvas.drawBitmap(backright,1785, 150, null);
-        canvas.drawBitmap(backright,1785, 175, null);
-        canvas.drawBitmap(backright,1785, 200, null);
-        canvas.drawBitmap(backright,1785, 225, null);
-        canvas.drawBitmap(backright,1785, 250, null);
-        canvas.drawBitmap(backright,1785, 275, null);
-        canvas.drawBitmap(backright,1785, 300, null);
-        canvas.drawBitmap(backright,1785, 325, null);
 
-        //Draw text??
-        canvas.drawText("# played:" + state.getNumInPile() , 1500, 1500, purple);
+            //scale the bitmaps
+            //back of the card
+            back = Bitmap.createScaledBitmap(back, 125, 175, false);
+            backleft = Bitmap.createScaledBitmap(backleft, 175, 125, false);
+            backright = Bitmap.createScaledBitmap(backright, 175, 125, false);
 
-        //Time to do the pile!
-        canvas.drawBitmap(dalmuti,1500, 1500, null);
+            //Actual cards
+            dalmuti = Bitmap.createScaledBitmap(dalmuti, 125, 175, false);
+
+            //draw the top hand
+            canvas.drawBitmap(back, 450, 10, null);
+            canvas.drawBitmap(back, 550, 10, null);
+            canvas.drawBitmap(back, 650, 10, null);
+            canvas.drawBitmap(back, 750, 10, null);
+            canvas.drawBitmap(back, 850, 10, null);
+            canvas.drawBitmap(back, 950, 10, null);
+            canvas.drawBitmap(back, 1050, 10, null);
+            canvas.drawBitmap(back, 1150, 10, null);
+            canvas.drawBitmap(back, 1250, 10, null);
+            canvas.drawBitmap(back, 1350, 10, null);
+
+            //draw the left players hand
+            canvas.drawBitmap(backleft, 10, 100, null);
+            canvas.drawBitmap(backleft, 10, 125, null);
+            canvas.drawBitmap(backleft, 10, 150, null);
+            canvas.drawBitmap(backleft, 10, 175, null);
+            canvas.drawBitmap(backleft, 10, 200, null);
+            canvas.drawBitmap(backleft, 10, 225, null);
+            canvas.drawBitmap(backleft, 10, 250, null);
+            canvas.drawBitmap(backleft, 10, 275, null);
+            canvas.drawBitmap(backleft, 10, 300, null);
+            canvas.drawBitmap(backleft, 10, 325, null);
+
+
+            //draw the right players hand
+            canvas.drawBitmap(backright, 1785, 100, null);
+            canvas.drawBitmap(backright, 1785, 125, null);
+            canvas.drawBitmap(backright, 1785, 150, null);
+            canvas.drawBitmap(backright, 1785, 175, null);
+            canvas.drawBitmap(backright, 1785, 200, null);
+            canvas.drawBitmap(backright, 1785, 225, null);
+            canvas.drawBitmap(backright, 1785, 250, null);
+            canvas.drawBitmap(backright, 1785, 275, null);
+            canvas.drawBitmap(backright, 1785, 300, null);
+            canvas.drawBitmap(backright, 1785, 325, null);
+
+            //Draw text for num card
+            canvas.drawText("# played:" + state.getNumInPile(),  750, 500, purple);
+
+            //Time to do the pile!
+            canvas.drawBitmap(dalmuti, 900, 250, null);
+
+            //TODO Fix what card is placed on the pile based on what card was just played
+            if(state.getRankInPile() == 0){
+                canvas.drawBitmap(dalmuti, 900, 250, null);
+            }
 
     }
 }
