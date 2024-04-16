565b9b1ace8f6a1409d6fe1275e476c1bb7ecc24
DropT <best27@up.edu>
2024-04-15 17:56:11 -0700

    number of jester and cards selected fixed officially and shows changes on tablet after playing

--- app/src/main/java/edu/up/cs301/GreatDalmuti/GDHumanPlayer.java
+++ app/src/main/java/edu/up/cs301/GreatDalmuti/GDHumanPlayer.java
@@ -46,6 +46,7 @@ public class GDHumanPlayer extends GameHumanPlayer implements OnClickListener {
 	//selected number of cards
 	int n;
 	int j;
+	String nString;
 
 	//pass and hold buttons
 	private Button passButton;
@@ -169,52 +170,78 @@ public class GDHumanPlayer extends GameHumanPlayer implements OnClickListener {
 		else if(button.getId() == R.id.jester){
 			 c = 13;
 			cardsNum.setText("" + state.getDeck().get(playerNum).get(c));
+			nString = state.getDeck().get(playerNum).get(c).toString();
+			n = Integer.parseInt(nString);
 		} else if(button.getId() == R.id.one){
 			c = 1;
 			cardsNum.setText("" + state.getDeck().get(playerNum).get(c));
+			nString = state.getDeck().get(playerNum).get(c).toString();
+			n = Integer.parseInt(nString);
 		} else if(button.getId() == R.id.two){
 			c = 2;
 			cardsNum.setText("" + state.getDeck().get(playerNum).get(c));
+			nString = state.getDeck().get(playerNum).get(c).toString();
+			n = Integer.parseInt(nString);
 		} else if(button.getId() == R.id.three){
 			c = 3;
 			cardsNum.setText("" + state.getDeck().get(playerNum).get(c));
+			nString = state.getDeck().get(playerNum).get(c).toString();
+			n = Integer.parseInt(nString);
 		} else if(button.getId() == R.id.four){
 			c = 4;
 			cardsNum.setText("" + state.getDeck().get(playerNum).get(c));
+			nString = state.getDeck().get(playerNum).get(c).toString();
+			n = Integer.parseInt(nString);
 		} else if(button.getId() == R.id.five){
 			c = 5;
 			cardsNum.setText("" + state.getDeck().get(playerNum).get(c));
+			nString = state.getDeck().get(playerNum).get(c).toString();
+			n = Integer.parseInt(nString);
 		} else if(button.getId() == R.id.six){
 			c = 6;
 			cardsNum.setText("" + state.getDeck().get(playerNum).get(c));
+			nString = state.getDeck().get(playerNum).get(c).toString();
+			n = Integer.parseInt(nString);
 		} else if(button.getId() == R.id.seven){
 			c = 7;
 			cardsNum.setText("" + state.getDeck().get(playerNum).get(c));
+			nString = state.getDeck().get(playerNum).get(c).toString();
+			n = Integer.parseInt(nString);
 		} else if(button.getId() == R.id.eight){
 			c = 8;;
 			cardsNum.setText("" + state.getDeck().get(playerNum).get(c));
+			nString = state.getDeck().get(playerNum).get(c).toString();
+			n = Integer.parseInt(nString);
 		} else if(button.getId() == R.id.nine){
 			c = 9;
 			cardsNum.setText("" + state.getDeck().get(playerNum).get(c));
+			nString = state.getDeck().get(playerNum).get(c).toString();
+			n = Integer.parseInt(nString);
 		} else if(button.getId() == R.id.ten){
 			c = 10;
 			cardsNum.setText("" + state.getDeck().get(playerNum).get(c));
+			nString = state.getDeck().get(playerNum).get(c).toString();
+			n = Integer.parseInt(nString);
 		} else if(button.getId() == R.id.eleven){
 			c = 11;
 			cardsNum.setText("" + state.getDeck().get(playerNum).get(c));
+			nString = state.getDeck().get(playerNum).get(c).toString();
+			n = Integer.parseInt(nString);
 		} else if(button.getId() == R.id.twelve){
 			c = 12;
 			cardsNum.setText("" + state.getDeck().get(playerNum).get(c));
+			nString = state.getDeck().get(playerNum).get(c).toString();
+			n = Integer.parseInt(nString);
 		}
 
 
 		//more or less cards
 		 if(button.getId() == R.id.addbutton){
-			String nString = cardsNum.getText().toString();
+			nString = cardsNum.getText().toString();
 			n = Integer.parseInt(nString) + 1;
 			cardsNum.setText("" + n);
 		} else if(button.getId() == R.id.minusbutton){
-			 String nString = cardsNum.getText().toString();
+			 nString = cardsNum.getText().toString();
 			 n = Integer.parseInt(nString) - 1;
 			 cardsNum.setText("" + n);
 		}
@@ -229,6 +256,21 @@ public class GDHumanPlayer extends GameHumanPlayer implements OnClickListener {
 			j = Integer.parseInt(nString) - 1;
 			jesterSelected.setText("" + j);
 		}
+
+		//updates number of cards
+		jesterNum.setText("" + state.getDeck().get(playerNum).get(13));
+		oneNum.setText("" + state.getDeck().get(playerNum).get(1));
+		twoNum.setText("" + state.getDeck().get(playerNum).get(2));
+		threeNum.setText("" + state.getDeck().get(playerNum).get(3));
+		fourNum.setText("" + state.getDeck().get(playerNum).get(4));
+		fiveNum.setText("" + state.getDeck().get(playerNum).get(5));
+		sixNum.setText("" + state.getDeck().get(playerNum).get(6));
+		sevenNum.setText("" + state.getDeck().get(playerNum).get(7));
+		eightNum.setText("" + state.getDeck().get(playerNum).get(8));
+		nineNum.setText("" + state.getDeck().get(playerNum).get(9));
+		tenNum.setText("" + state.getDeck().get(playerNum).get(10));
+		elevenNum.setText("" + state.getDeck().get(playerNum).get(11));
+		twelveNum.setText("" + state.getDeck().get(playerNum).get(12));
 
 
 //		// I cannot overstate how long I took me to figure out this needed to be called on
